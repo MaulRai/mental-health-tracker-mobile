0b978838ba71c30d768c1dfc3f2917565c79538a
MaulRai <muhammadraihanofc@gmail.com>
2024-11-13 18:29:12 +0700

    done tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -64,7 +64,7 @@ class MyHomePage extends StatelessWidget {
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
-                      'Welcome to Mental Health Tracker',
+                      'Welcome to Mental Health Tracker!',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
